8d3faa30752b16cfa5d12268c891b1e9caa0b65b
Yandr <102684712+Yandr2022@users.noreply.github.com>
2023-09-05 18:00:29 +0300

    Creating simple Hibernate application

---
+++ src/main/java/by/Yandr2022/spring/learn/App.java
@@ -0,0 +1,26 @@
+package by.Yandr2022.spring.learn;
+
+
+import by.Yandr2022.spring.learn.model.Person;
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+             Session session = sessionFactory.getCurrentSession()) {
+
+            session.beginTransaction();
+
+            System.out.println(session.get(Person.class, 1).getName());
+
+        }
+
+    }
+}
